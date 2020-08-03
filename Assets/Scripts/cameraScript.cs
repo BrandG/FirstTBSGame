a2aae6592bd68e0af854ce4a40580ab5b240bfaa
Brand Gamblin <brandg@gmail.com>
2020-08-03 14:35:23 -0400

    Initial Commit

---
+++ Assets/Scripts/cameraScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraScript : MonoBehaviour
+{
+    private float movementSpeed = 0.25f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis ("Horizontal");
+        float v = Input.GetAxis ("Vertical");
+
+        GameObject thisObj = this.gameObject;
+        Vector3 thisPos = thisObj.transform.position;
+        thisPos.x += (h + v) * movementSpeed;
+        thisPos.z += (v - h) * movementSpeed;
+        thisObj.transform.position = thisPos;
+    }
+}
---
+++ Assets/Scripts/mainScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainScript : MonoBehaviour
+{
+
+    private float tileSize = 40.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitGround();
+        InitCamera();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void InitCamera() {
+        GameObject camObj = GameObject.Find ("Main Camera");
+        Vector3 camPos = camObj.transform.position;
+        camPos.x += tileSize/2.0f;
+        camPos.z += tileSize/2.0f;
+        camObj.transform.position = camPos;
+    }
+
+    // build the basic ground tiles.
+    void InitGround() 
+    {
+        GameObject groundObj = GameObject.Find ("GroundTile_0_0");
+        Transform groundPos = groundObj.transform;
+
+        for (int i=0; i<tileSize; i++) {
+            for (int j=0; j<tileSize; j++) {
+                GameObject newObj = Instantiate(groundObj);
+                newObj.name = "GroundTile_"+i+"_"+j;
+                Vector3 newPos = groundPos.position;
+                newPos.x= i*1.0f;
+                newPos.z= j*1.0f;
+                newPos.y += Random.Range(-0.25f,0.25f);
+                newObj.gameObject.transform.position = newPos;
+            }
+        }
+
+    }
+}
